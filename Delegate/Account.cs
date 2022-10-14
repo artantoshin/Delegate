6b58c7eb89c753fe167f424fdd49d7591cb2aafc
art.antoshin <art.antoshin1992@gmail.com>
2022-10-14 22:40:41 +0400

    Добавьте файлы проекта.

---
+++ Delegate/Account.cs
@@ -0,0 +1,28 @@
+﻿public delegate void AccountHandler(string message);
+public class Account
+{
+    int sum;
+    AccountHandler? taken;
+    public Account(int sum) => this.sum = sum;
+    // Регистрируем делегат
+    public void RegisterHandler(AccountHandler del)
+    {
+        taken += del;
+    }
+    // Отмена регистрации делегата
+    public void UnregisterHandler(AccountHandler del)
+    {
+        taken -= del; // удаляем делегат
+    }
+    public void Add(int sum) => this.sum += sum;
+    public void Take(int sum)
+    {
+        if (this.sum >= sum)
+        {
+            this.sum -= sum;
+            taken?.Invoke($"Со счета списано {sum} у.е.");
+        }
+        else
+            taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿/*DoOperation(5, 4, Add);         // 9
+DoOperation(5, 4, Subtract);    // 1
+DoOperation(5, 4, Multiply);    // 20
+
+void DoOperation(int a, int b, Operation op)
+{
+    Console.WriteLine(op(a, b));
+}
+int Add(int x, int y) => x + y;
+int Subtract(int x, int y) => x - y;
+int Multiply(int x, int y) => x * y;
+
+delegate int Operation(int x, int y);*/
+
+Account account = new Account(200);
+// Добавляем в делегат ссылку на методы
+account.RegisterHandler(PrintSimpleMessage);
+account.RegisterHandler(PrintColorMessage);
+// Два раза подряд пытаемся снять деньги
+account.Take(100);
+account.Take(150);
+
+// Удаляем делегат
+account.UnregisterHandler(PrintColorMessage);
+// снова пытаемся снять деньги
+account.Take(50);
+
+void PrintSimpleMessage(string message) => Console.WriteLine(message);
+void PrintColorMessage(string message)
+{
+    // Устанавливаем красный цвет символов
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    // Сбрасываем настройки цвета
+    Console.ResetColor();
+}
